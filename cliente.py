134a5e31b24fa667b0f41389fad2285cc17b008e
Marcos <marcosmeireles015@gmail.com>
2023-09-26 21:34:26 -0300

    add

--- cliente.py
+++ cliente.py
@@ -5,26 +5,8 @@ host = 'localhost'
 port = 12345
 
 
-def resposta1():
-    resposta = 'a)'
+def escolha(resposta):
     return s.sendall(resposta.encode())
-
-
-def resposta2():
-    resposta = 'b)'
-    return s.sendall(resposta.encode())
-
-
-def resposta3():
-    resposta = 'c)'
-    return s.sendall(resposta.encode())
-
-
-def resposta4():
-    resposta = 'd)'
-    return s.sendall(resposta.encode())
-
-
 
 def mostrar_questoes(pergunta):
 
@@ -39,24 +21,33 @@ def mostrar_questoes(pergunta):
     frame3 = tk.Frame(root)
     frame4 = tk.Frame(root)
     frame5 = tk.Frame(root)
+    frame6 = tk.Frame(root)
 
-    titulo = tk.Label(frame1, text=questao, justify=tk.CENTER)
+    titulo = tk.Label(frame1, text=questao, justify=tk.CENTER, wraplength=500)
     titulo.pack(side=tk.LEFT)
 
-    alternativa1 = tk.Button(frame2, textvariable=respostas[1], command=resposta1, width=50, justify=tk.LEFT)
+    alternativa1 = tk.Button(frame2, text=respostas[1], command=escolha('a)'), width=50, justify=tk.LEFT, wraplength=350)
     alternativa1.pack()
 
-    alternativa2 = tk.Button(frame3, textvariable=respostas[2], command=resposta2, width=50, justify=tk.LEFT)
+    alternativa2 = tk.Button(frame3, text=respostas[2], command=escolha('b)'), width=50, justify=tk.LEFT, wraplength=350)
     alternativa2.pack()
 
-    alternativa3 = tk.Button(frame4, textvariable=respostas[3], command=resposta3, width=50, justify=tk.LEFT)
+    alternativa3 = tk.Button(frame4, text=respostas[3], command=escolha('c)'), width=50, justify=tk.LEFT, wraplength=350)
     alternativa3.pack()
 
-    alternativa4 = tk.Button(frame5, textvariable=respostas[5], command=resposta4, width=50, justify=tk.LEFT)
+    alternativa4 = tk.Button(frame5, text=respostas[4], command=escolha('d)'), width=50, justify=tk.LEFT, wraplength=350)
     alternativa4.pack()
 
+    enviar = tk.Button(frame6,text='Enviar',command=root.destroy, width=50, justify=tk.LEFT, wraplength=350)
+    enviar.pack()
+
     frame1.pack(padx=1, pady=1)
-    frame2.pack(padx=10, pady=10)
+    frame2.pack(pady=10, padx=10)
+    frame3.pack(padx=10, pady=10)
+    frame4.pack(padx=10, pady=10)
+    frame5.pack(padx=10, pady=10)
+    frame6.pack(padx=10, pady=10)
+
 
     root.mainloop()
 
@@ -64,7 +55,7 @@ def mostrar_questoes(pergunta):
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((host, port))
     cont = 0
-    while True:
+    while cont < 10:
         question = s.recv(1024).decode()
         mostrar_questoes(question)
         if not question:
